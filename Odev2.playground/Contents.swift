242bce80de3a6d15b7b20feaac2d6c7bd76b3be7
zekigklr <77938037+zekigklr@users.noreply.github.com>
2023-01-07 20:52:13 +0300

    Odev2
    Design

--- Odev2.playground/Contents.swift
+++ Odev2.playground/Contents.swift
@@ -1,30 +1,27 @@
 import UIKit
-
+//--------------------------------------------------------------------------//
 //Soru 1
-
+//--------------------------------------------------------------------------//
 class KenarSayisi {
     var ks: Int?
     
     func icAcilarToplami(ks: Int) -> Int {
         if ks > 2 {
             let islem = ((ks - 2) * 180 )
-            print("İc acılar toplamı : \(islem) derecedir")
+            print("Soru 1 : İc acılar toplamı : \(islem) derecedir")
             return islem
         }else{
-            print("Minimum 3 giriniz.")
+            print("Soru 1 : Minimum 3 giriniz.")
             return ks
         }
-        
     }
-    
 }
-
 var sonuc = KenarSayisi()
 sonuc.icAcilarToplami(ks: 10)
 sonuc.icAcilarToplami(ks: 2)
-
+//--------------------------------------------------------------------------//
 //Soru 2
-
+//--------------------------------------------------------------------------//
 class MaasHesabi {
     var gun: Int?
     
@@ -32,81 +29,75 @@ class MaasHesabi {
         let islem = gun * 80
         if islem > 1600 {
             let mesai = (gun - 20) * 160 + 1600
-            print("Mesai ücretleriyle beraber \(mesai)₺ kazandınız.")
+            print("Soru 2 : Mesai ücretleriyle beraber \(mesai)₺ kazandınız.")
             return mesai
         }else {
-            print("Mesai yok. Kazanılan ücret \(islem)₺'dir.")
+            print("Soru 2 : Mesai yok. Kazanılan ücret \(islem)₺'dir.")
             return islem
         }
-        
-        
     }
-   
-    
 }
 var maas = MaasHesabi()
 maas.maasHesabi(gun: 21)
 
 maas.maasHesabi(gun: 12)
-
+//--------------------------------------------------------------------------//
 //Soru 3
-
+//--------------------------------------------------------------------------//
 class Kota {
     var kota: Int?
     
     func hesaplama(kota: Int) -> Int {
         let ucret = kota * 2
-        
         if ucret > 100 {
             let ucret = (kota - 50 ) * 4 + 100
-            print("Kota kullanım tutarınız aşım ücreti ile beraber \(ucret)₺'dir")
+            print("Soru 3 : Kota kullanım tutarınız aşım ücreti ile beraber \(ucret)₺'dir")
             return ucret
         }
         else{
-            print("Kota kullanım tutarınız \(ucret)₺'dir")
+            print("Soru 3 : Kota kullanım tutarınız \(ucret)₺'dir")
             return ucret
         }
     }
 }
+
 var kota = Kota()
+
 kota.hesaplama(kota: 50)
 kota.hesaplama(kota: 51)
-
+//--------------------------------------------------------------------------//
 //Soru 4
-
-
+//--------------------------------------------------------------------------//
 class Fahrenhiet {
     var celcius: Int?
     
     func change(celcius: Double) -> Double {
         let fahrenhiet = celcius * 1.8 + 32
         
-        print("\(celcius) celcius = \(fahrenhiet) fahrenheit")
+        print("Soru 4 : \(celcius) celcius = \(fahrenhiet) fahrenheit")
         return fahrenhiet
-        
     }
 }
 
 var fahrenhiet = Fahrenhiet()
 fahrenhiet.change(celcius: 100)
-
+//--------------------------------------------------------------------------//
 //Soru 5
-
+//--------------------------------------------------------------------------//
 class Dikdortgen {
     var x: Int?
     var y: Int?
     
-    
     func cevre(x: Int, y: Int){
         let cevre = 2 * (x + y)
-        print("Dikdörtgenin cevre uzunlugu toplamı = \(cevre).")
+        print("Soru 5 : Dikdörtgenin cevre uzunlugu toplamı = \(cevre).")
     }
 }
 var f = Dikdortgen()
 f.cevre(x: 10, y: 5)
-
+//--------------------------------------------------------------------------//
 //Soru 6
-
+//--------------------------------------------------------------------------//
 func faktoriyel( sayi : Int ) -> Int {
         var x = 1
         
@@ -114,17 +105,12 @@ func faktoriyel( sayi : Int ) -> Int {
             x *= i
         }
         return x
-        
     }
-
-    print ( faktoriyel(sayi: 5))
-    
-
-
+    print ("Soru 6 : \(faktoriyel(sayi: 5))")
+//--------------------------------------------------------------------------//
 //Soru 7
-
+//--------------------------------------------------------------------------//
 func buraya(kelimegirin: String) -> Int{
-    
     var x = 0
     
     for i in kelimegirin {
@@ -132,8 +118,10 @@ func buraya(kelimegirin: String) -> Int{
             x += 1
         }
     }
-    print("Metnin içinde \(x) tane 'a' harfi bulunuyor.")
+    print("Soru 7 : Metnin içinde \(x) tane 'a' harfi bulunuyor.")
     return x
+
 }
 
 buraya(kelimegirin: "Bunu yaparken biraz zorlandım ama güzeldi :)")
+
