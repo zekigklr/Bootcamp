61d90ff3deab0a17ce2eb810cd4d1bbfc8f4527b
zekigklr <77938037+zekigklr@users.noreply.github.com>
2023-04-02 17:58:39 +0300

    Update APICaller.swift

--- Netflix Clone/Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Netflix Clone/Managers/APICaller.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Constants {
     static let API_KEY = "697d439ac993538da4e3e60b54e762cd"
     static let baseURL = "https://api.themoviedb.org"
-}
+}//http://kasimadalan.pe.hu/yemekler/tumYemekleriGetir.php
 
 enum APIError: Error {
     case failedTogetData
